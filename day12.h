5949123aa292989027fbaf34c7d1ae7f59a60269
Dinesh Manajipet <saidinesh5@gmail.com>
2020-04-30 14:01:29 +0530

    Day 12

---
+++ day12.h
@@ -0,0 +1,55 @@
+#pragma once
+
+/*
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+    If x == y, both stones are totally destroyed;
+    If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+Note:
+
+    1 <= stones.length <= 30
+    1 <= stones[i] <= 1000
+*/
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::priority_queue<int> queue;
+
+        for(int s: stones)
+            queue.push(s);
+
+        while (!queue.empty()) {
+            int y = queue.top(); queue.pop();
+
+            // If 1 stone is left, return that
+            if (queue.empty()) {
+                return y;
+            }
+
+            int x = queue.top(); queue.pop();
+
+            // x <= y since this is priority queue. So put it back in the queue
+            if (y > x) {
+                queue.push(y-x);
+            }
+        }
+
+        return 0;
+    }
+};
