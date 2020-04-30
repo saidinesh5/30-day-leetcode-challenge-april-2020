1916f4597deebfe81d60f48dd1afac55285dc220
Dinesh Manajipet <saidinesh5@gmail.com>
2020-04-30 14:31:44 +0530

    Day 26

---
+++ day26.h
@@ -0,0 +1,55 @@
+#pragma once
+
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+If there is no common subsequence, return 0.
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
+Explanation: The longest common subsequence is "ace" and its length is 3.
+
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+
+Constraints:
+
+    1 <= text1.length <= 1000
+    1 <= text2.length <= 1000
+    The input strings consist of lowercase English characters only.
+*/
+
+class Solution {
+public:
+
+    int longestCommonSubsequence(const string& text1, const string& text2) {
+        const int n = text1.size(), m = text2.size();
+        std::vector<std::vector<int>> dp(2, std::vector<int>(m + 1, 0));
+        int previousRow = 0, currentRow = 1;
+
+        for (int row = 1; row <= n; row++) {
+            currentRow = row % 2;
+            for (int column = 1; column <= m; column++) {
+                dp[currentRow][column] = text1[row-1] == text2[column-1] ? 1 + dp[previousRow][column-1]
+                                                                         : max(dp[previousRow][column],
+                                                                           dp[currentRow][column-1]);
+            }
+            previousRow = currentRow;
+        }
+
+        return dp[currentRow][m];
+    }
+};
